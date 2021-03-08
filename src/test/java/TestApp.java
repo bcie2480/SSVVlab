14c6b38c16eb8399d5412045941d98d22f49a170
Bogdan Cristina <cristina.bogdan9996@yahoo.com>
2021-03-08 13:22:30 +0200

    first

---
+++ src/test/java/TestApp.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+public class TestApp {
+
+    @Test
+    public void test1()
+    {
+        assertTrue(true);
+    }
+
+    @Test
+    public void test2()
+    {
+        assertFalse(false);
+    }
+
+}
